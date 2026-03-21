3adacafd2ed07d024e0601a0cd49471ed66c462c
Shiivee <leomichel619@gmail.com>
2026-03-21 23:39:50 +0100

    retrying commit

---
+++ src/game/system.js
@@ -0,0 +1,32 @@
+export const ConfigGame = Object.freeze({
+  FPS: 60,
+  FPS_RATE: 1000 / ConfigGame.FPS,
+  CANVAS_WIDTH: 1920,
+  CANVAS_HEIGHT: 1080,
+  GameState: {
+    MENU_SCENE: 0,
+    STARTER_SELECT_SCENE: 1,
+    SAVE_SCENE: 2,
+    BATTLE_SCENE: 3
+  }
+});
+export const ConfigUI = Object.freeze({
+  Text: {
+    Smallest: 8,
+    Smaller: 10,
+    Small: 12,
+    Medium: 18,
+    Big: 22,
+    Bigger: 24,
+    Biggest: 26
+  },
+  Outline: {
+    Smallest: 1,
+    Smaller: 2,
+    Small: 3,
+    Medium: 4,
+    Big: 5,
+    Bigger: 6,
+    Biggest: 7
+  }
+});
